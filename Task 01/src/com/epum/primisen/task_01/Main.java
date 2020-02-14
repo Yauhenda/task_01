26743cc0dfd458418a5b874d2f5afa5226434cff
Nadia <primisen17@gmail.com>
2020-02-14 14:43:24 +0300

    Create class Main. Main describe menu and read user data.

--- Task 01/src/com/epum/primisen/task_01/Main.java
+++ Task 01/src/com/epum/primisen/task_01/Main.java
@@ -1,8 +1,25 @@
 package com.epum.primisen.task_01;
 
+import com.epum.primisen.task_01.menu.Menu;
+import com.epum.primisen.task_01.menu.MenuPrinter;
+import com.epum.primisen.task_01.util.ConsoleReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Menu menu = new Menu();
+        menu.setDescriptionOfMenu("Choose a number of a game that is located below for start a game.");
+        menu.setGameList("Monty Hall paradox");
+        menu.setGameList("Birth paradox");
+        menu.setGameList("Task of picky bride");
+
+        MenuPrinter menuPrinter = new MenuPrinter(menu);
+        menuPrinter.print();
+
+        ConsoleReader consoleReader = new ConsoleReader();
+        consoleReader.readNumFromOneToDiapason(menu.getNumberOfGame());
+
+
     }
 }
