b31415d68f17bb8c1f4edefb3c2eeb6456ee9412
Nadia <primisen17@gmail.com>
2020-02-14 11:48:51 +0300

    Add class PickyBride which implements interface Menu

---
+++ Task 01/src/com/epum/primisen/task_01/task_of_picky_bride/PickyBride.java
@@ -0,0 +1,11 @@
+package com.epum.primisen.task_01.task_of_picky_bride;
+
+import com.epum.primisen.task_01.menu.Menu;
+
+public class PickyBride implements Menu {
+
+    @Override
+    public String takeTaskName(){
+        return "Picky bride";
+    }
+}
