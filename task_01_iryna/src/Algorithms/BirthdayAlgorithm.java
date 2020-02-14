41c39f3ae893cd38971646ac55ffb89f1402b3d8
Luiryna <luiryna322@gmail.com>
2020-02-14 16:52:50 +0300

    compositionOfProb instead of k variable

--- task_01_iryna/src/Algorithms/BirthdayAlgorithm.java
+++ task_01_iryna/src/Algorithms/BirthdayAlgorithm.java
@@ -5,14 +5,14 @@ public class BirthdayAlgorithm implements Algorithm {
     private static final int NUMBER_OF_PEOPLE = 23;
 
     public double getResult() {
-        double k = 1.0;
+        double compositionOfProb = 1.0;
 
         for (int i = DAYS_IN_A_YEAR - NUMBER_OF_PEOPLE; i <= DAYS_IN_A_YEAR - 1; i++) {
-            k = k * i;
-            k = k / 365;
+            compositionOfProb = compositionOfProb * i;
+            compositionOfProb = compositionOfProb / 365;
         }
 
-        return 100 * (1 - k);
+        return 100 * (1 - compositionOfProb);
     }
 
     public void printResult() {
