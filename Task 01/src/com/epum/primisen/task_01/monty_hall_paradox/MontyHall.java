90b3312e09820cf9f8272bf09b9cf02b0dab13a0
Nadia <primisen17@gmail.com>
2020-02-14 11:40:04 +0300

    Class MontyHall implements interface Menu

--- Task 01/src/com/epum/primisen/task_01/monty_hall_paradox/MontyHall.java
+++ Task 01/src/com/epum/primisen/task_01/monty_hall_paradox/MontyHall.java
@@ -2,7 +2,10 @@ package com.epum.primisen.task_01.monty_hall_paradox;
 
 import com.epum.primisen.task_01.menu.Menu;
 
-public class MontyHall{
+public class MontyHall implements Menu {
 
-
+    @Override
+    public String takeTaskName(){
+        return "Monty Hall paradox";
+    }
 }
