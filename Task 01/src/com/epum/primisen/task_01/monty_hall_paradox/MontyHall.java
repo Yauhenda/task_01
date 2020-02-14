fcaa066d23eb4d937475bcf8bbeef676744a7599
Nadia <primisen17@gmail.com>
2020-02-14 13:43:02 +0300

    Remove implementation Interface MenuInterface from class MontyHall

--- Task 01/src/com/epum/primisen/task_01/monty_hall_paradox/MontyHall.java
+++ Task 01/src/com/epum/primisen/task_01/monty_hall_paradox/MontyHall.java
@@ -1,11 +1,5 @@
 package com.epum.primisen.task_01.monty_hall_paradox;
 
-import com.epum.primisen.task_01.menu.MenuInterface;
+public class MontyHall {
 
-public class MontyHall implements MenuInterface {
-
-    @Override
-    public String takeTaskName(){
-        return "Monty Hall paradox";
-    }
 }
