2f5a0b14e79f50eae9afa6f00e99d6c3cb3e005b
Nadia <primisen17@gmail.com>
2020-02-14 11:46:57 +0300

    Add class BirthParadox which implements interface Menu

---
+++ Task 01/src/com/epum/primisen/task_01/birth_paradox/BirthParadox.java
@@ -0,0 +1,11 @@
+package com.epum.primisen.task_01.birth_paradox;
+
+import com.epum.primisen.task_01.menu.Menu;
+
+public class BirthParadox implements Menu {
+
+    @Override
+    public String takeTaskName(){
+        return "Birth paradox";
+    }
+}
