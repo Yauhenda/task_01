6febadca04ad9fb7e1b8f0159ca2f69bdbba9be0
Nadia <primisen17@gmail.com>
2020-02-14 14:38:07 +0300

    Remove magic number from MontyHall.java

--- Task 01/src/com/epum/primisen/task_01/monty_hall_paradox/MontyHall.java
+++ Task 01/src/com/epum/primisen/task_01/monty_hall_paradox/MontyHall.java
@@ -2,4 +2,38 @@ package com.epum.primisen.task_01.monty_hall_paradox;
 
 public class MontyHall {
 
+    public static final int NUMBER_OF_DOOR = 3;
+
+    private boolean[] door;
+
+    public MontyHall(){
+        door = new boolean[NUMBER_OF_DOOR];
+    }
+
+    public boolean[] getDoor() {
+        return door;
+    }
+
+    public void setDoor(boolean[] door) {
+        this.door = door;
+    }
+
+    public boolean getDoor(int index){//обработать исключение, если значение индекса недопустимо
+        return door[index];
+    }
+
+    public void setDoor(int index, boolean value){//обработать исключение, если значение индекса недопустимо
+        door[index] = value;
+    }
+
+    //переопределить
+//    @Override
+//    public int hashCode(){
+//        int result = 107;
+//        for (int i = 0; i < door.length; i++) {
+//            door[i] == true
+//        }
+//
+//        return
+//    }
 }
