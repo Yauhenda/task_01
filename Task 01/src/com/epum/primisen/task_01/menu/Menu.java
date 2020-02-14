9873cf6fc5a57bb5a3e68fce10a71cbee1a223eb
Nadia <primisen17@gmail.com>
2020-02-14 13:14:11 +0300

    Add class Menu. This class for describe of application for user

---
+++ Task 01/src/com/epum/primisen/task_01/menu/Menu.java
@@ -0,0 +1,34 @@
+package com.epum.primisen.task_01.menu;
+
+import java.util.HashMap;
+
+public class Menu {
+
+    private String descriptionOfMenu;
+    private HashMap<Integer, String> gameList;
+
+    public Menu(){
+        gameList = new HashMap<Integer, String>();
+    }
+
+    public String getDescriptionOfMenu() {
+        return descriptionOfMenu;
+    }
+
+    public void setDescriptionOfMenu(String descriptionOfMenu) {
+        this.descriptionOfMenu = descriptionOfMenu;
+    }
+
+    public HashMap<Integer, String> getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(HashMap<Integer, String> gameList) {
+        this.gameList = gameList;
+    }
+
+    public void setGameList(String game) {
+        Integer key = (Integer)gameList.size() + 1;
+        this.gameList.put(key, game);
+    }
+}
